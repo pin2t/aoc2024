de4e83dd42b06c53ea9831d6fc54538cb2844e79
pin2t <pin2t@mail.ru>
2024-12-01 09:21:50 +0400

    day 1

--- 01.go
+++ 01.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import	"fmt"
+import	"math"
+import	"slices"
 
+func main() {
+	var left, right []int = make([]int, 0), make([]int, 0)
+	for {
+		var l, r int
+		if n, _ := fmt.Scan(&l, &r); n < 2 {
+			break
+		}
+		left = append(left, l)
+		right = append(right, r)
+	}
+	slices.Sort(left)
+	slices.Sort(right)
+	var total, similarity int
+	for i, _ := range left {
+		total += int(math.Abs(float64(left[i] - right[i])))
+		var c int
+		for _, n := range right {
+			if left[i] == n { c++ }
+		}
+		similarity += left[i] * c
+	}
+	fmt.Println(total, similarity)
 }
