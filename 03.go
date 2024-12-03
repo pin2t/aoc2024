9dbf691e34f83880833ee1166d6e922c3329a6d0
pin2t <pin2t@mail.ru>
2024-12-03 11:08:39 +0400

    day 3

---
+++ 03.go
@@ -0,0 +1,27 @@
+package main
+
+import "bufio"
+import "os"
+import "regexp"
+import "strconv"
+import "fmt"
+
+func main() {
+	var reMul = regexp.MustCompile("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+	var scanner = bufio.NewScanner(os.Stdin)
+	var muls, dmuls int64
+	var do = true
+	for scanner.Scan() {
+		for _, sub := range reMul.FindAllStringSubmatch(scanner.Text(), -1) {
+			if sub[0][0:3] == "mul" {
+				n1, _ := strconv.ParseInt(sub[1], 10, 32)
+				n2, _ := strconv.ParseInt(sub[2], 10, 32)
+				muls += n1 * n2
+				if do { dmuls += n1 * n2 }
+			} else {
+				do = sub[0] == "do()"
+			}
+		}
+	}
+	fmt.Println(muls, dmuls)
+}
