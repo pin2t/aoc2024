51dc5f57e1dea2ce14636cb83c3dae5baec136dc
pin2t <pin2t@mail.ru>
2024-12-03 14:25:02 +0400

    day 3

--- 03.go
+++ 03.go
@@ -3,25 +3,24 @@ package main
 import "bufio"
 import "os"
 import "regexp"
-import "strconv"
 import "fmt"
 
 func main() {
-	var reMul = regexp.MustCompile("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
+	var reMul = regexp.MustCompile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)")
 	var scanner = bufio.NewScanner(os.Stdin)
-	var muls, dmuls int64
+	var muls [2]int
 	var do = true
 	for scanner.Scan() {
-		for _, sub := range reMul.FindAllStringSubmatch(scanner.Text(), -1) {
-			if sub[0][0:3] == "mul" {
-				var n1, _ = strconv.ParseInt(sub[1], 10, 32)
-				var n2, _ = strconv.ParseInt(sub[2], 10, 32)
-				muls += n1 * n2
-				if do { dmuls += n1 * n2 }
+		for _, s := range reMul.FindAllString(scanner.Text(), -1) {
+			if s[0:3] == "mul" {
+				var n1, n2 int
+				fmt.Sscanf(s, "mul(%d,%d)", &n1, &n2)
+				muls[0] += n1 * n2
+				if do { muls[1] += n1 * n2 }
 			} else {
-				do = sub[0] == "do()"
+				do = s == "do()"
 			}
 		}
 	}
-	fmt.Println(muls, dmuls)
+	fmt.Println(muls)
 }
