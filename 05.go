3781f89f4ecb8f6be24db31eaf22b8c662c6f859
pin2t <pin2t@mail.ru>
2024-12-05 10:35:16 +0400

    day 5

--- 05.go
+++ 05.go
@@ -8,39 +8,34 @@ import "strings"
 import "regexp"
 import "strconv"
 
-func numbers(s string) []int {
-	var re = regexp.MustCompile("\\d+")
-	var result []int
-	for _, ss := range re.FindAllString(s, -1) {
-		n, _ := strconv.ParseInt(ss, 10, 32)
-		result = append(result, int(n))
-	}
-	return result
-}
-
 func main() {
-	var scanner = bufio.NewScanner(os.Stdin)
 	var orders [][]int
+	var cmp = func (left, right int) int {
+		for _, order := range orders {
+			if order[0] == right && order[1] == left { return 1 }
+			if order[0] == left && order[1] == right { return -1 }
+		}
+		return 0
+	}
+	var re = regexp.MustCompile("\\d+")
+	var scanner = bufio.NewScanner(os.Stdin)
 	var sums [2]int
 	for scanner.Scan() {
 		var line = scanner.Text()
+		var numbers []int
+		for _, ss := range re.FindAllString(line, -1) {
+			n, _ := strconv.ParseInt(ss, 10, 32)
+			numbers = append(numbers, int(n))
+		}
 		if strings.Contains(line, "|") {
-			orders = append(orders, numbers(line))
+			orders = append(orders, numbers)
 		} else {
-			var nns = numbers(line)
-			if len(nns) == 0 { continue } // skip empty line
-			var cmp = func (left, right int) int {
-				for _, order := range orders {
-					if order[0] == right && order[1] == left { return 1 }
-					if order[0] == left && order[1] == right { return -1 }
-				}
-				return 0
-			}
-			if slices.IsSortedFunc(nns, cmp) {
-				sums[0] += nns[len(nns) / 2]
+			if len(numbers) == 0 { continue } // skip empty line
+			if slices.IsSortedFunc(numbers, cmp) {
+				sums[0] += numbers[len(numbers) / 2]
 			} else {
-				slices.SortFunc(nns, cmp)
-				sums[1] += nns[len(nns) / 2]
+				slices.SortFunc(numbers, cmp)
+				sums[1] += numbers[len(numbers) / 2]
 			}
 		}
 	}
