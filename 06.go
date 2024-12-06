848793b73bdd2fdc2ee43d389e777c166dc75201
pin2t <pin2t@mail.ru>
2024-12-06 10:03:46 +0400

    day 6

--- 06.go
+++ 06.go
@@ -8,22 +8,23 @@ import "fmt"
 type pos struct {x, y int}
 
 var _map [][]rune
-var directions = []pos{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}
+var directions = []pos{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}         // up, right, down, left (in order of 90 degree turns)
 
+// return true if guard stuck in a loop. Second item in a pair is total steps done
 func loop(guard pos, di int) (bool, int) {
 	var visited = make(map[pos]bool, 10000)
-	var onPath int                                            // number of steps on already existing path
+	var onVisited int                                            // number of steps on already visited points
 	visited[guard] = true
 	for {
 		var next = pos{guard.x + directions[di % 4].x, guard.y + directions[di % 4].y}
 		if next.x < 0 || next.y < 0 || next.x >= len(_map[guard.y]) || next.y >= len(_map) {
 			return false, len(visited)
 		}
-		if onPath > 100 { return true, len(visited) }         // assume a loop when stepping on path for some time
+		if onVisited > 100 { return true, len(visited) }         // assume a loop when stepping on already visited for some time
 		if _map[next.y][next.x] == '#' {
 			di++
 		} else {
-			if visited[next] { onPath++ } else { onPath = 0 }
+			if visited[next] { onVisited++ } else { onVisited = 0 }
 			guard = next
 		}
 		visited[guard] = true
