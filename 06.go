8415cecf938461a97ba75ad354f1ba471fba5b94
pin2t <pin2t@mail.ru>
2024-12-06 10:46:41 +0400

    day 6

--- 06.go
+++ 06.go
@@ -8,24 +8,24 @@ import "fmt"
 type pos struct {x, y int}
 
 var _map [][]rune
-var directions = []pos{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}         // up, right, down, left (in order of 90 degree turns)
+var directions = []pos{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}              // up, right, down, left (in order of 90 degree turns)
 
-// return true if guard stuck in a loop. Second item in returned pair is total steps done
+// return true if guard stuck in a loop. Second item in returned pair is number of visited points
 func loop(guard pos, di int) (bool, int) {
 	var visited = make(map[pos]bool, 10000)
-	var onVisited int                                            // number of steps on already visited points
+	var nsteps = 1                                                    // number of total steps (including non-unique)
 	visited[guard] = true
 	for {
 		var next = pos{guard.x + directions[di % 4].x, guard.y + directions[di % 4].y}
 		if next.x < 0 || next.y < 0 || next.x >= len(_map[guard.y]) || next.y >= len(_map) {
 			return false, len(visited)
 		}
-		if onVisited > 100 { return true, len(visited) }         // assume a loop when stepping on already visited for some time
+		if nsteps - len(visited) > 1000 { return true, len(visited) } // assume a loop when stepping on already visited for some time
 		if _map[next.y][next.x] == '#' {
 			di++
 		} else {
-			if visited[next] { onVisited++ } else { onVisited = 0 }
 			guard = next
+			nsteps++
 		}
 		visited[guard] = true
 	}
@@ -41,8 +41,8 @@ func main() {
 		if p > 0 { guard = pos{p, y} }
 		y++
 	}
-	var _, steps = loop(guard, 0)
-	fmt.Print(steps)
+	var _, visited = loop(guard, 0)
+	fmt.Print(visited)
 	var nloops int
 	for y, row := range _map {
 		for x, c := range row {
